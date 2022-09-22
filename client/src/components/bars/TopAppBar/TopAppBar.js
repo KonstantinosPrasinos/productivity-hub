3572f913eecda10b9ab6ab42d31019f6afe6d3af
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2022-09-22 16:43:31 +0300

    Delete unused component Fix top app bar having wrong title Fix top app bar button rendering incorrectly

--- client/src/components/bars/TopAppBar/TopAppBar.js
+++ client/src/components/bars/TopAppBar/TopAppBar.js
@@ -1,6 +1,5 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useContext, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import styles from './topAppBar.module.scss';
@@ -12,21 +11,37 @@ const TopAppBar = () => {
   const location = useLocation();
 
   const screenSizeContext = useContext(ScreenSizeContext);
+
   const [isMainPage, setIsMainPage] = useState(false);
-  const currentLocation = location.pathname.slice(location.pathname.indexOf('/', 1) + 1, location.pathname.length).replaceAll('/', '');
-  const capitalizedLocation = currentLocation.charAt(0).toUpperCase() + currentLocation.slice(1);
+  const [pageName, setPageName] = useState('');
 
   useEffect(() => {
-    if (location.pathname.split('/').length - 1 > 1) {
-      setIsMainPage(false);
-    } else {
-      setIsMainPage(true);
+
+
+    switch (location.pathname) {
+      case '/':
+        setIsMainPage(true);
+        setPageName('Home');
+        break;
+      case '/settings':
+        setIsMainPage(true);
+        setPageName('Timer');
+        break;
+      case '/timer':
+        setIsMainPage(true);
+        setPageName('Settings');
+        break;
+      default:
+        setIsMainPage(false);
+        const currentLocation = location.pathname.slice(location.pathname.indexOf('/', 1) + 1, location.pathname.length).replaceAll('/', '');
+        setPageName((currentLocation.charAt(0).toUpperCase() + currentLocation.slice(1)).replaceAll('-', ' '));
+        break;
     }
   }, [location])
 
   return (screenSizeContext.state === 'small' && <div className={`${styles.container} ${!isMainPage ? styles.center : ''}`}>
-    {isMainPage && <IconButton onClick={() => navigate(-1)}><ArrowBackIcon /></IconButton>}
-    <div className={`Headline`}>{capitalizedLocation}</div>
+    {!isMainPage && <IconButton onClick={() => navigate(-1)}><ArrowBackIcon /></IconButton>}
+    <div className={`Headline`}>{pageName}</div>
   </div>);
 };
 
