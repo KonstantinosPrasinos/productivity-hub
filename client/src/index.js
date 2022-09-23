9d7f09a092a31fdc7e20f89195256c5849ba96ec
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2022-09-23 12:36:43 +0300

    Delete styles.css Move button styling to index.scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
 import { store } from './state/store';
 import { Provider } from 'react-redux';
