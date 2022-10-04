aa2b2e49cc26232f4a2faba890c84a84fc9a6e42
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2022-10-04 17:41:13 +0300

    Style some components Fix collapsing container animation bug Fix collapsing container overflow bug

--- client/src/components/utilities/CollapsibleContainer/CollapsibleContainer.js
+++ client/src/components/utilities/CollapsibleContainer/CollapsibleContainer.js
@@ -4,17 +4,33 @@ import styles from './CollapsibleContainer.module.scss';
 
 const CollapsibleContainer = ({children, isVisible}) => {
     return (
-        <AnimatePresence>
-            {isVisible && <motion.div
-                className={`Stack-Container ${styles.container}`}
-                initial={{height: 0, overflowY: 'hidden'}}
-                animate={{height: "auto", transitionEnd: {overflowY: 'visible'}}}
-                exit={{height: 0, overflowY: 'hidden'}}
-                transition={{type: 'tween'}}
-            >
-                {children}
-            </motion.div>}
-        </AnimatePresence>
+        <div>
+            <AnimatePresence>
+                {isVisible && <motion.div
+                    className={`Stack-Container ${styles.container}`}
+                    initial={"collapsed"}
+                    animate={"extended"}
+                    exit={"collapsed"}
+                    variants={{
+                        collapsed: {
+                            height: 0,
+                            padding: 0,
+                            overflowY: 'hidden'
+                        },
+                        extended: {
+                            height: 'auto',
+                            padding: '10px 0',
+                            transitionEnd: {
+                                overflowY: 'visible'
+                            }
+                        }
+                    }}
+                    transition={{type: 'tween'}}
+                >
+                    {children}
+                </motion.div>}
+            </AnimatePresence>
+        </div>
     );
 };
 
