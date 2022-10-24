0a331aa53b5d8f87d8748991d149d8fb35453f50
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2022-10-24 19:08:17 +0300

    Update nav bar so that its contained in one file Delete Sidebar component Ddlete Bottombar component

--- client/src/components/bars/NavBar/NavBar.js
+++ client/src/components/bars/NavBar/NavBar.js
@@ -1,17 +1,78 @@
-import { useContext } from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import styles from './NavBar.module.scss'
-
-import { ScreenSizeContext } from '../../../context/ScreenSizeContext';
-import SideBar from "../SideBar/SideBar";
-import BottomBar from "../BottomBar/BottomBar";
+import IconButton from "../../buttons/IconButton/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Button from "../../buttons/Button/Button";
+import AddIcon from "@mui/icons-material/Add";
+import {useLocation, useNavigate} from "react-router-dom";
+import {MiniPagesContext} from "../../../context/MiniPagesContext";
 
 const NavBar = () => {
-    const screenSizeContext = useContext(ScreenSizeContext);
+    const navigate = useNavigate();
+
+    const location = useLocation();
+
+    const [selected, setSelected] = useState(null);
+
+    const miniPagesContext = useContext(MiniPagesContext);
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/':
+            case '/home':
+                setSelected('home')
+                break;
+            case '/timer':
+                setSelected('timer')
+                break;
+            case '/tasks':
+                setSelected('tasks')
+                break;
+            case '/settings':
+                setSelected('settings')
+                break;
+            default:
+                break;
+        }
+    }, [location]);
 
     return (
         <div className={styles.container}>
-            {screenSizeContext.state === 'small' ? <BottomBar /> : <SideBar />}
+            <div className={`${styles.navBar}`}>
+                <IconButton
+                    onClick={() => navigate('/', {replace: true})}
+                    selected={selected === 'home'}
+                    setSelected={() => setSelected('home')}
+                >
+                    <HomeIcon sx={{fontSize: '1.5em'}}/>
+                </IconButton>
+                {/*<IconButton onClick={() => navigate('/timer', {replace: true})} selected={selected === 'timer'}*/}
+                {/*            setSelected={() => setSelected('timer')}><TimerIcon sx={{fontSize: '1.5em'}}/></IconButton>*/}
+                <IconButton
+                    onClick={() => {
+                        setSelected('tasks');
+                        navigate('/tasks', {replace: true});
+                    }}
+                    selected={selected === 'tasks'}
+                >
+                    <FormatListBulletedIcon sx={{fontSize: '1.5em'}}/>
+                </IconButton>
+                <IconButton
+                    onClick={() => navigate('/settings', {replace: true})} selected={selected === 'settings'}
+                    setSelected={() => setSelected('settings')}
+                >
+                    <SettingsIcon sx={{fontSize: '1.5em'}}/>
+                </IconButton>
+                <Button
+                    type='square'
+                    onClick={() => miniPagesContext.dispatch({type: 'ADD_PAGE', payload: {type: 'new-task'}})}
+                >
+                    <AddIcon sx={{fontSize: '1.5em'}}/>
+                </Button>
+            </div>
         </div>
 
     );
