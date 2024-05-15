df90be3ffa02e92bf979c16f09412843cee755b4
Konstantinos Prasinos <74455528+KonstantinosPrasinos@users.noreply.github.com>
2024-05-16 01:23:02 +0200

    feat(client:): add chips for filtering with category

--- client/src/components/utilities/TaskList/TaskList.jsx
+++ client/src/components/utilities/TaskList/TaskList.jsx
@@ -1,56 +1,132 @@
-import React from 'react';
+import React, { useMemo, useState } from "react";
 import styles from "./TaskList.module.scss";
-import {AnimatePresence, motion} from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Task from "@/components/indicators/Task/Task.jsx";
+import Chip from "@/components/buttons/Chip/Chip";
+import { useGetCategories } from "@/hooks/get-hooks/useGetCategories";
 
 const variants = {
-    hidden: {opacity: 0},
-    visible: {
-        opacity: 1,
-        transition: {staggerChildren: 0.05}
-    },
-    exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } }
-}
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.05 },
+  },
+  exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } },
+};
 
 const childVariants = {
-    hidden: { opacity: 0, y: 50, scale: 0.8 },
-    visible: { opacity: 1, y: 0, scale: 1 },
-    exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } }
-}
+  hidden: { opacity: 0, y: 50, scale: 0.8 },
+  visible: { opacity: 1, y: 0, scale: 1 },
+  exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } },
+};
 
-const TaskList = ({tasks = []}) => {
-    return (
-        <motion.div
-            className={`Stack-Container ${styles.leftSide}`}
+const CategoryChips = ({ categoryFilter, setCategoryFilter }) => {
+  const { data: categories } = useGetCategories();
 
-            variants={variants}
-            initial={"hidden"}
-            animate={"visible"}
-            exit={"exit"}
-        >
-            {/*
+  const toggleSelected = (category) => {
+    if (categoryFilter.includes(category)) {
+      setCategoryFilter((current) =>
+        current.filter((tempCategory) => tempCategory._id != category._id)
+      );
+    } else {
+      setCategoryFilter((current) => [...current, category]);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.categoryChipContainer}>
+        {categoryFilter.map((category) => (
+          <Chip
+            size={"small"}
+            key={category._id}
+            selected={category._id}
+            value={category._id}
+            setSelected={() => toggleSelected(category)}
+          >
+            <div className="Horizontal-Flex-Container">
+              <div
+                className={`${styles.categoryChipColor} ${category.color}`}
+              ></div>
+              {category.title}
+            </div>
+          </Chip>
+        ))}
+        {categories
+          .filter((category) => !categoryFilter.includes(category))
+          .map((category) => (
+            <Chip
+              size={"small"}
+              key={category._id}
+              selected={null}
+              value={category._id}
+              setSelected={() => toggleSelected(category)}
+            >
+              <div className="Horizontal-Flex-Container">
+                <div
+                  className={`${styles.categoryChipColor} ${category.color}`}
+                ></div>
+                {category.title}
+              </div>
+            </Chip>
+          ))}
+      </div>
+    </>
+  );
+};
+
+const TaskList = ({ tasks = [] }) => {
+  const [filter, setFilter] = useState([]);
+
+  const filteredTasks = useMemo(() => {
+    if (filter.length == 0) return tasks;
+
+    return tasks.filter((task) =>
+      filter.map((tempFilter) => tempFilter._id).includes(task.category)
+    );
+  }, [filter, tasks]);
+
+  return (
+    <motion.div
+      variants={variants}
+      initial={"hidden"}
+      animate={"visible"}
+      exit={"exit"}
+      className={styles.container}
+    >
+      <CategoryChips categoryFilter={filter} setCategoryFilter={setFilter} />
+      <div className={`Stack-Container ${styles.leftSide}`}>
+        {/*
                 Animate Presence is needed here to set initial to true.
                 Otherwise, the stagger doesn't work on list view because of the switch container.
             */}
-            <AnimatePresence initial={true}>
-                {tasks.length === 0 &&
-                    <motion.div
-                        initial={"hidden"}
-                        animate={"visible"}
-                        exit={"exit"}
-                        variants={childVariants}
-                        className={`Empty-Indicator-Container`}
-                    >
-                        No tasks for now
-                    </motion.div>
-                }
-                {tasks.length > 0 && tasks.map((task) => !task.hasOwnProperty('tasks') ?
-                    (<Task key={task._id} tasks={[task]}></Task>) :
-                    (<Task key={task.tasks[0]._id} tasks={task.tasks}></Task>)
-                )}
-            </AnimatePresence>
-        </motion.div>
-    );
+        <AnimatePresence initial={true} mode="popLayout">
+          {filteredTasks.length === 0 && (
+            <motion.div
+              initial={"hidden"}
+              animate={"visible"}
+              exit={"exit"}
+              variants={childVariants}
+              className={`Empty-Indicator-Container`}
+            >
+              No tasks for now
+            </motion.div>
+          )}
+          {filteredTasks.length > 0 &&
+            filteredTasks.map((task) =>
+              !task.hasOwnProperty("filteredTasks") ? (
+                <Task key={task._id} tasks={[task]}></Task>
+              ) : (
+                <Task
+                  key={task.filteredTasks[0]._id}
+                  tasks={task.filteredTasks}
+                ></Task>
+              )
+            )}
+        </AnimatePresence>
+      </div>
+    </motion.div>
+  );
 };
 
 export default TaskList;
