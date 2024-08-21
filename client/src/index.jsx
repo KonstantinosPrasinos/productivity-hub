3f9db5cea2dae02616c771c0ae9adcb7c6abe9ee
Konstantinos Prasinos <74455528+KonstantinosPrasinos@users.noreply.github.com>
2024-08-21 21:02:45 +0300

    feat(capacitor): install capacitor dependencies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import App from './App';
-import AlertsContextProvider from './context/AlertsContext';
+import React from "react";
+import App from "./App";
+import AlertsContextProvider from "./context/AlertsContext";
 import MiniPagesContextProvider from "./context/MiniPagesContext";
 import UserContextProvider from "./context/UserContext";
-import {BrowserRouter} from "react-router-dom";
-import {QueryClientProvider, QueryClient} from "react-query";
-import {createRoot} from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const queryClient = new QueryClient();
 
 const container = document.getElementById("root");
-const root = createRoot(container)
+const root = createRoot(container);
 
-root.render(// <React.StrictMode>
+root.render(
+  // <React.StrictMode>
+  <GoogleOAuthProvider
+    clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+    onScriptLoadError={() => console.log("failed to load script")}
+  >
     <AlertsContextProvider>
-        <MiniPagesContextProvider>
-            <UserContextProvider>
-                <BrowserRouter>
-                    <QueryClientProvider client={queryClient}>
-                        <App/>
-                    </QueryClientProvider>
-                </BrowserRouter>
-            </UserContextProvider>
-        </MiniPagesContextProvider>
+      <MiniPagesContextProvider>
+        <UserContextProvider>
+          <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </BrowserRouter>
+        </UserContextProvider>
+      </MiniPagesContextProvider>
     </AlertsContextProvider>
-    // </React.StrictMode>
+  </GoogleOAuthProvider>,
+  // </React.StrictMode>
 );
