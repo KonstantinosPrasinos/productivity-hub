e2d09416f0c8bdf03ac62e0ee578844a0e5a2459
Konstantinos Prasinos <74455528+KonstantinosPrasinos@users.noreply.github.com>
2024-09-18 16:38:50 +0300

    revert: revert offline changes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const cors = require('cors')
-const MongoDBStore = require('connect-mongodb-session')(session);
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const cors = require("cors");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const {loginUser, googleLogin} = require('./controllers/userController');
-const User = require('./models/userSchema');
+const { loginUser, googleLogin } = require("./controllers/userController");
+const User = require("./models/userSchema");
 
-const userRoutes = require('./routes/userRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-const settingsRoutes = require('./routes/settingsRoutes');
-const groupRoutes = require('./routes/groupRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const securityRoutes = require('./routes/securityRoutes');
-const taskHistoryRoutes = require('./routes/entryRoutes');
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const settingsRoutes = require("./routes/settingsRoutes");
+const groupRoutes = require("./routes/groupRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const securityRoutes = require("./routes/securityRoutes");
+const taskHistoryRoutes = require("./routes/entryRoutes");
 // const {Strategy: GoogleStrategy} = require("passport-google-oauth20");
 
 // Express app
@@ -24,45 +24,48 @@ const app = express();
 
 // Session store
 const sessionStore = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
-    collection: 'sessions'
-})
+  uri: process.env.MONGODB_URI,
+  collection: "sessions",
+});
 
 // Middleware
 app.use(express.json());
 app.use(
-    session({
-        store: sessionStore,
-        secret: process.env.SECRET,
-        resave: false,
-        saveUninitialized: false,
-        proxy: true,
-        name: 'ProductivityHubCookie',
-        cookie: {
-            secure: true,
-            maxAge: 30 * 24 * 60 * 60 * 1000, // One month,
-            httpOnly: false,
-            sameSite: 'none'
-        },
+  session({
+    store: sessionStore,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    proxy: true,
+    name: "ProductivityHubCookie",
+    cookie: {
+      secure: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // One month,
+      httpOnly: false,
+      sameSite: "none",
+    },
 
-        // proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
-        // name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
-        // cookie: {
-        //     httpOnly: false,
-        //     sameSite: 'none'
-        // }
-    })
+    // proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
+    // name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
+    // cookie: {
+    //     httpOnly: false,
+    //     sameSite: 'none'
+    // }
+  }),
 );
 
 const corsOptions = {
-    origin: ["https://productivity-hub-website.vercel.app", "http://localhost:5173"],
-    methods: ["POST", "GET"],
-    credentials: true,
+  origin: [
+    "https://productivity-hub-website.vercel.app",
+    "http://localhost:5173",
+  ],
+  methods: ["POST", "GET"],
+  credentials: true,
 };
 
 app.get("/", (req, res) => {
-    res.json("Hello there")
-})
+  res.json("Hello there");
+});
 
 app.use(cors(corsOptions));
 
@@ -70,50 +73,54 @@ app.use(cors(corsOptions));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-    done(null, user._id);
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
 });
 
-passport.deserializeUser(function(user, done) {
-    User.findById(user, (err, user) => {
-        done(null, user);
-    })
+passport.deserializeUser(function (user, done) {
+  User.findById(user, (err, user) => {
+    done(null, user);
+  });
 });
 
 passport.use(loginUser);
 passport.use(googleLogin);
 
 // Routes
-app.use('/api/user', userRoutes);
-app.use('/api/settings', settingsRoutes)
-app.use('/api/task', taskRoutes);
-app.use('/api/group', groupRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/security', securityRoutes);
-app.use('/api/entry', taskHistoryRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/task", taskRoutes);
+app.use("/api/group", groupRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/security", securityRoutes);
+app.use("/api/entry", taskHistoryRoutes);
 
 app.use((err, req, res, next) => {
-    if (err.message) {
-        return res.status(400).json({message: err.message})
-    }
-    next();
-})
+  if (err.message) {
+    return res.status(400).json({ message: err.message });
+  }
+  next();
+});
 
 if (!process.env.MONGODB_URI) {
-    throw new Error("Please provide a MONGODB_URI in the .env file");
+  throw new Error("Please provide a MONGODB_URI in the .env file");
 }
 
 // Connect to database
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        if (process.env.NODE_ENV !== 'test') {
-            app.listen(process.env.PORT, () => {
-                console.log('connected to database and listening to port: ', process.env.PORT);
-            });
-        }
-    })
-    .catch(error => {
-        console.log(error);
-    });
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    if (process.env.NODE_ENV !== "test") {
+      app.listen(process.env.PORT, () => {
+        console.log(
+          "connected to database and listening to port: ",
+          process.env.PORT,
+        );
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 module.exports = app;
